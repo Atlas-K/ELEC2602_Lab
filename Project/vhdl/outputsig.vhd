ea70d6c2ad6b0a3a70e62487f72afa369a9a15b0
Rona Wang <rwan8547@uni.sydney.edu.au>
2020-05-21 12:58:12 +1000

    Change state numbers in outputsig.vhd according to state_machine diagram

--- Project/vhdl/outputsig.vhd
+++ Project/vhdl/outputsig.vhd
@@ -3,7 +3,7 @@ USE ieee.std_logic_1164.all;
 
 ENTITY outputsig is
 	port (
-				state: IN std_logic_vector(4 downto 0);
+				state: IN std_logic_vector(3 downto 0);
 				 : OUT std_logic_vector(2 downto 0); --(TODO): Add component wire names
 				reg0sel, reg1sel: OUT std_logic_vector(3 downto 0);
 				 : OUT std_logic_vector(2 downto 0);
@@ -12,7 +12,7 @@ ENTITY outputsig is
 				 : OUT std_logic_vector(2 downto 0);
 				 : OUT std_logic_vector(2 downto 0);
 				 : OUT std_logic_vector(1 downto 0);
-				mode, rw_mode : OUT std_logic
+				mode, rw_mode, alu_reg1_in, alu_reg2_out: OUT std_logic
 			);
 END;
 ARCHITECTURE behavioural of outputsig is
@@ -20,46 +20,61 @@ ARCHITECTURE behavioural of outputsig is
 			process(state)
 			begin
 				case state is
-					when "10001" => --During load. Also need to change R_y.
+					when "0000" => --During reset
+						--
+					when "0001" => --During store instructions
+						--
+					when "0010" => --During PC load instruction 0
+						--
+					when "0011" => --During PC instruction to instruction register
+						--
+					when "0100" => --During PC instruction to instruction register & PC increments by 1
+						--
+					when "0101" => --During Instruction register instruction to Decoder
+						--
+					when "0110" => --During load. Also need to change R_y.
 						decoder_out <= '1'; 
 						reg0sel <= R_y; 
 						mode <= '1'; 
 						rw_mode <= '0';
-					when "10010" => --During load_or_move
+					when "0111" => --During load_or_move
 						reg0sel <= R_x; 
 						reg1sel <= R_y; 
 						mode <= '0'; 
 						rw_mode <= 0/1;	
-					when "10100" => --During add1, see if you have to change names / values or not
-						reg0sel <= R_y; 
-						mode <= '1'; 
-						rw_mode <= '1'; 
-						alu_mode <= '1';	
-					when "10011" => --During add_or_xor, need to change values if needed
+					when "1000" => --During add_or_xor, need to change values if needed
 						reg0sel <= R_x; 
 						mode <= '1'; 
 						rw_mode <= '1'; 
 						alu_reg1_in <= '1';	
-					when "10110" => --During xor, see if type of values / names changes.
+					when "1001" => --During add1, see if you have to change names / values or not
+						reg0sel <= R_y; 
+						mode <= '1'; 
+						rw_mode <= '1'; 
+						alu_mode <= '1';	
+					when "1010" => --During xor, see if type of values / names changes.
 						reg0sel <= R_y; 
 						mode <= '1'; 
 						rw_mode <= '1'; 
 						alu_mode <= '0'';	
-					when "10111" => --During add_or_xor1, see/change
+					when "1011" => --During add_or_xor1, see/change
 						reg0sel <= R_x; 
 						mode <= '1'; 
 						rw_mode <= '0'; 
 						alu_reg2_out <= '1'; 
-					when "11000" => --During ldpc, see/change
+					when "1100" => --During ldpc, see/change
 						reg0sel <= R_x; 
 						mode <= '1'; 
 						rw_mode <= '0'; 
 						pc_out <= '1'; 		
-					when "11001" => --During branch, see/change
+					when "1101" => --During branch, see/change
 						reg0sel <= R_x; 
 						mode <= '1'; 
 						rw_mode <= '1'; 
-						pc_in <= 1;		
+						pc_in <= 1;
+					when "1110" => --End state
+						
+						;		
 					--(TODO): Implement FLOOR as part of ALU?
 					when "" =>  <= ; <= ; <= ; <= ; <= ; 			--These ones have been left blank as many other states are prone to change until the other components have been finalised.
 					
