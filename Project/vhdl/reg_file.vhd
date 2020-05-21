a090b041082b6e2ed34ec283e0276e85e42cc9ff
Moses Kim <61644589+Atlas-K@users.noreply.github.com>
2020-05-21 15:15:25 +0900

    Add files via upload

--- Project/vhdl/reg_file.vhd
+++ Project/vhdl/reg_file.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
+USE ieee.std_logic_1164.ALL;
 
 
 ------------------------------------------------------------------------------------------------------------
@@ -28,10 +29,11 @@ use ieee.std_logic_unsigned.all;
 
 entity reg_file is
    generic(ADDR_SIZE: integer := 3;  -- 8 regs
-           DATA_SIZE: integer := 16; -- 16 bit data
+           DATA_SIZE: integer := 16 -- 16 bit data
            );
    
-    port(clk: in std_logic;
+    port(
+		clk: in std_logic;
         reg0sel: in std_logic_vector(ADDR_SIZE - 1 downto 0); -- 2bit address
         reg1sel: in std_logic_vector(ADDR_SIZE - 1 downto 0); -- 2bit address
         mode: in std_logic;                                   -- 0/1
@@ -45,28 +47,29 @@ end;
 architecture Behavior of reg_file is
     type reg is array(0 to 2**ADDR_SIZE - 1) of std_logic_vector(DATA_SIZE - 1 downto 0);
     signal reg_bus: reg;
-
+begin
    w: process(clk)
-      variable write_reg: unsigned range 0 to 2**ADDR_SIZE - 1;
-      variable read_reg: unsigned range 0 to 2**ADDR_SIZE - 1;
+      variable write_reg: integer range 0 to 2**ADDR_SIZE - 1;
+      variable read_reg: integer range 0 to 2**ADDR_SIZE - 1;
    begin
         if clk = '1' and clk'event then
-        internal: if mode = '0' then
-            write_reg := conv_unsigned(reg0sel);
-            read_reg := conv_unsigned(reg1sel);
-            reg_bus(write_reg) <= reg_bus(read_reg);
-            output <= ('others' => 'Z');
+			if mode = '0' then
+				write_reg := conv_integer(reg0sel);
+				read_reg := conv_integer(reg1sel);
+				reg_bus(write_reg) <= reg_bus(read_reg);
+				output <= (others => 'Z');
 
-        external_in: elsif mode = '1' then
-            if rw_mode = '0' then
-                write_reg := conv_unsigned(reg0sel);
-                reg_bus(write_reg) <= input;
-                output <= ('others' => 'Z');
+			elsif mode = '1' then
+				if rw_mode = '0' then
+					write_reg := conv_integer(reg0sel);
+					reg_bus(write_reg) <= input;
+					output <= (others => 'Z');
+				end if;
+			end if;
 
-        external_out: elsif rw_mode = '1' then
-                read_reg := conv_unsigned(reg0sel);
+        elsif rw_mode = '1' then
+                read_reg := conv_integer(reg0sel);
                 output <= reg_bus(read_reg);
-            end if;
         end if;
-    end if;
-end reg_file;
+	end process;
+end Behavior;
