e0978e26c3f1752bb23a6b8d571232f8e913f1ad
LordAbys <62635259+LordAbys@users.noreply.github.com>
2020-05-23 16:26:49 +1000

    Update next_state.vhd

--- Project/vhdl/next_state.vhd
+++ Project/vhdl/next_state.vhd
@@ -5,9 +5,7 @@ ENTITY next_state is
 	port (	--I don't think that any of this useful except for state, f1f0, reset, next_state. I will update this in the end.
 	
 				state: IN std_logic_vector(3 downto 0); 		--Current State.
-				f1f0:	 IN std_LOGIC_VECTOR(2 downto 0);		--The operation to be performed (Add, Move, XOR, etc) given by OPCODE.
-				reg0:	 IN std_LOGIC_VECTOR( downto );			--The first register
-				reg1:	 IN std_LOGIC_VECTOR( downto );			--The second register
+				f1f0:	 IN std_LOGIC_VECTOR(2 downto 0);		--The operation to be performed (Add, Move, XOR, etc) given by OPCODE. (Connect this to op_code of decoder in FSM)
 				reset: IN std_logic;									--Everyone is familiar with this devil.
 				next_state : OUT std_logic_vector(3 downto 0)--The next state that will be output
 				);
@@ -20,8 +18,8 @@ begin
 		process(state, f1f0, w, reset) is
 				
 		begin
-				
-				if (reset = 1) then							--If statements are used here instead of "case" as the events are not mutually exclusive. Check this website for mutually exclusive: https://electronics.stackexchange.com/questions/73387/difference-between-if-else-and-case-statement-in-vhdl
+		--If statements are used here instead of "case" as the events are not mutually exclusive. Check this website for mutually exclusive: https://electronics.stackexchange.com/questions/73387/difference-between-if-else-and-case-statement-in-vhdl
+				if (reset = 1) then							
 					next_state <= "0000";
 				elsif (state = "0000") then							--Path: Start -> Store Instruction
 					next_state <= "0001";
@@ -33,28 +31,28 @@ begin
 					next_state <= "0101";
 				elsif (state = "0101" and f1f0 = "000") then		--Path: Instruction Register Instruction to Decoder -> Kill Clock?? (Termination OPCODE)
 					next_state <= "1110"
-				elsif (state = "0101" and f1f0 = "010") then		--Path: Instruction Register Instruction to Decoder -> load_or_move (move only)
+				elsif (state = "0101" and f1f0 = "010") then		--Path: Instruction Register Instruction to Decoder -> move (move only)
 					next_state <= "0111";
 				elsif (state = "0101" and f1f0 = "001") then		--Path: Instruction Register Instruction to Decoder -> load
 					next_state <= "0110";
-				elsif (state = "0110") then							--Path: load -> load_or_move
-					next_state <= "0111";
-				elsif (state = "0111") then							--Path: load_or_move -> PC Instruction to instruction register & PC increments by 1
-					next_state <= "0100"									
-				elsif (state = "0101" and f1f0 = "011") then		--Path: Instruction Register Instruction to Decoder -> add_or_xor (add only)
+				elsif (state = "0110") then							--Path: load -> load1
+					next_state <= "1110";								--Excuse the break in pattern. I had to make these changes after creating this file.
+				elsif (state = "1110") then							--Path: load1 -> PC Instruction to instruction register & PC increments by 1
+					next_state <= "0100";
+				elsif (state = "0111") then							--Path: move (move only) -> PC Instruction to instruction register & PC increments by 1
+					next_state <= "0100";									
+				elsif (state = "0101" and f1f0 = "011") then		--Path: Instruction Register Instruction to Decoder -> add_or_xor_sub (add only)
 					next_state <= "1000";
-				elsif (state = "0101" and f1f0 = "101") then		--Path: Instruction Register Instruction to Decoder -> add_or_xor (xor only)
+				elsif (state = "0101" and f1f0 = "101") then		--Path: Instruction Register Instruction to Decoder -> add_or_xor_sub (xor only)
 					next_state <= "1000";
-				elsif (state = "1000" and f1f0 = "011") then		--Path: add_or_xor (add only) -> add1
+				elsif (state = "0101" and f1f0 = "100") then		--Path: Instruction Register Instruction to Decoder -> add_or_xor_sub (sub only)
+					next_state <= "1000"'
+				elsif (state = "1000") then							--Path: add_or_xor_sub (any) -> add_or_xor_sub1 (any)
 					next_state <= "1001";
-				elsif (state = "1000" and f1f0 = "101") then		--Path: add_or_xor (xor only) -> xor1
-					next_state <= "1010";
-				elsif (state = "1001") then							--Path: add1 -> add_or_xor 1
-					next_state <= "1011";
-				elsif (state = "1010") then							--Path: xor1 -> add_or_xor 1
-					next_state <= "1011";
-				elsif (state = "1011") then							--Path: add_or_xor 1 -> PC Instruction to instruction register & PC increments by 1
-					next_state <= "0100"
+				elsif (state = "1001") then							--Path: add_or_xor_sub1 (any) -> add_or_xor_sub2 (any)
+					next_state <= "1011"
+				elsif (state = "1011") then							--Path: add_or_xor2 (any) -> PC Instruction to instruction register & PC increments by 1
+					next_state <= "0100";
 				elsif (state = "0101" and f1f0 = "010") then		--Path: Instruction Register Instruction to Decoder -> ldpc
 					next_state <= "1100";
 				elsif (state = "1100") then							--Path: ldpc -> PC Instruction to instruction register & PC increments by 1
